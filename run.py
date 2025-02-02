1ec837dad4ac1812ff3115bc1c2c66b8327df2c5
Eduardoyet91 <morochoyet13@gmail.com>
2025-02-02 12:42:33 -0400

    Eduardo1

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 from app import create_app, db
 from app.models import CambioNumerico, Carrera, EstadoPeriodo, Informacion, IP, Periodo, RegistroEgresado, Usuario
+from flask_cors import CORS
 
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
